0da1b78557e1ec01775d3e10a8c90f0ef7733378
rionde <odhirxo@gmail.com>
2022-10-15 07:41:43 +0600

    refc: add settings to set non-sensitive info

---
+++ source/settings.ts
@@ -0,0 +1,22 @@
+export default {
+    guildId: "1028550020036825198",
+
+    emojis: {
+        forreal: "<:forreal:1029799729309167626>",
+        man: "<:man:1028553580300472432>"
+    },
+
+    channels: {
+        welcome: "1029789076410347541",
+        verify: "1029789024581337149"
+    },
+
+    roles: {
+        staff: "1028556732328333362"
+    },
+
+    paths: {
+        command: "./dist/commands/**/*.js",
+        listener: "./dist/listeners/**/*.js"
+    },
+}
